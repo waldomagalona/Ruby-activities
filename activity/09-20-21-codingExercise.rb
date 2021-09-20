88f18f21f1ab7522215cc13dbbf6db2184505744
waldomagalona <waldomagalona@gmail.com>
2021-09-20 19:39:58 +0800

    finish 9/20/21 coding exercise

--- activity/09-20-21-codingExercise.rb
+++ activity/09-20-21-codingExercise.rb
@@ -1,12 +1,20 @@
 #get smallest number in the array
-num = [34, 15, 88, 2]
-x = 0
-for n in num do
-    if x === 0
-        x = n
+num1 = [34, 15, 88, 2]
+num2 = [34, -345, -1, 100]
 
-    elsif x > n
-        x = n
+
+def getSmallest(num)
+    x = 0
+    num.each do |n|
+        if x === 0
+            x = n
+
+        elsif x > n
+            x = n
+        end
     end
+    puts x
 end
-puts x
+
+getSmallest(num1)
+getSmallest(num2)
